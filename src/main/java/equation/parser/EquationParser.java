fe13153cbe9a345618e0e1eda61eea99f664b189
KBuczko <kamil.buczko@nvt.pl>
2019-02-19 12:37:56 +0100

    EquationParser methods are now static

--- src/main/java/equation/parser/EquationParser.java
+++ src/main/java/equation/parser/EquationParser.java
@@ -7,13 +7,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EquationParser {
 
-    public TwoParamsAction parse(String _equation){
+    public static TwoParamsAction parse(String _equation){
         Equation equation = new Equation(_equation);
 
         return parseRecursivly(equation);
     }
 
-    private TwoParamsAction parseRecursivly(Equation equation) {
+    private static TwoParamsAction parseRecursivly(Equation equation) {
         if(equation.isSimple()){
             return equation.toTwoParamsAction();
         }
--- src/test/java/EquationParserTest.java
+++ src/test/java/EquationParserTest.java
@@ -27,25 +27,25 @@ public class EquationParserTest {
 
     @Test
     public void sumTest(){
-        TwoParamsAction out = new EquationParser().parse("5 + 12");
+        TwoParamsAction out = EquationParser.parse("5 + 12");
         assertEquals(out, new Sum(new Value(5L), new Value(12L)));
     }
 
     @Test
     public void divTest(){
-        TwoParamsAction out = new EquationParser().parse("82  /7");
+        TwoParamsAction out = EquationParser.parse("82  /7");
         assertEquals(out, new Div(new Value(82L), new Value(7L)));
     }
 
     @Test
     public void mulTest(){
-        TwoParamsAction out = new EquationParser().parse("3 * 97");
+        TwoParamsAction out = EquationParser.parse("3 * 97");
         assertEquals(out, new Mul(new Value(3L), new Value(97L)));
     }
 
     @Test
     public void subTest(){
-        TwoParamsAction out = new EquationParser().parse("21- 32");
+        TwoParamsAction out = EquationParser.parse("21- 32");
         assertEquals(out, new Sub(new Value(21L), new Value(32L)));
     }
 }
