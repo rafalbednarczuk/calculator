b66d09fde0b34aa7f6762e82b0b245a614602038
KBuczko <kamil.buczko@nvt.pl>
2019-02-20 09:48:12 +0100

    Working parser without bracelets

--- src/main/java/equation/parser/EquationParser.java
+++ src/main/java/equation/parser/EquationParser.java
@@ -1,25 +1,104 @@
 package equation.parser;
 
-import actions.base.TwoParamsAction;
+import actions.base.BaseAction;
+import actions.base.Value;
+import actions.basic.Div;
+import actions.basic.Mul;
+import actions.basic.Sub;
+import actions.basic.Sum;
+import equation.parser.exception.EquationWithoutOperatorException;
+import equation.parser.exception.WrongOperator;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+
+import static equation.parser.Operator.*;
+import static java.lang.Double.parseDouble;
 
 //Parsing String equation to TwoParamsAction
 @NoArgsConstructor
 public class EquationParser {
 
-    public static TwoParamsAction parse(String _equation){
-        Equation equation = new Equation(_equation);
-
-        return parseRecursivly(equation);
-    }
-
-    private static TwoParamsAction parseRecursivly(Equation equation) {
-        if(equation.isSimple()){
-            return equation.toTwoParamsAction();
+    public static BaseAction parse(String equation){
+        if(isSimple(equation)){
+            return  new Value(parseDouble(equation.trim()));
         }
 
-        return null;//todo
+        int operatorIndex = getOperatorIndex(equation);
+        Operator operator = parseOperator(equation.charAt(operatorIndex));
+
+        String leftOperand = equation.substring(0, operatorIndex);
+        String rightOperand = equation.substring(operatorIndex + 1);
+
+        return toTwoParamsAction(operator, parse(leftOperand), parse(rightOperand));
     }
 
+    private static BaseAction toTwoParamsAction(Operator operator, BaseAction leftOperand, BaseAction rightOperand) {
+        switch (operator) {
+            case SUM:
+                return new Sum(leftOperand, rightOperand);
+            case SUB:
+                return new Sub(leftOperand, rightOperand);
+            case MUL:
+                return new Mul(leftOperand, rightOperand);
+            case DIV:
+                return new Div(leftOperand, rightOperand);
+            default:
+                throw new WrongOperator("Could not process operator while parsing equation to Two Params Equation");
+        }
+    }
+
+    private static Operator parseOperator(char operator) {
+        switch(operator){
+            case '+':
+                return SUM;
+            case '-':
+                return SUB;
+            case '*':
+                return MUL;
+            case '/':
+                return DIV;
+            default:
+                throw new WrongOperator("Could not process operator while parsing char to enum");
+        }
+    }
+
+    private static boolean isSimple(@NotNull String equation) {
+        return equation.chars()
+                       .mapToObj(i -> (char) i)
+                       .noneMatch(e -> Arrays.asList('+', '-', '*', '/').contains(e));
+    }
+
+    private static int getOperatorIndex(@NotNull String equation) {
+        Integer operatorIndex;
+
+        int sumIndex = equation.indexOf('+');
+        int subIndex = equation.indexOf('-');
+
+        operatorIndex = getFirstOccurenceOfOperator(sumIndex, subIndex);
+        if (operatorIndex != -1) return operatorIndex;
+
+        int mulIndex = equation.indexOf('*');
+        int divIndex = equation.indexOf('/');
+
+        operatorIndex = getFirstOccurenceOfOperator(mulIndex, divIndex);
+        if (operatorIndex != -1) return operatorIndex;
+
+        throw new EquationWithoutOperatorException("Getting index of operator from equation that had none");
+    }
+
+    private static Integer getFirstOccurenceOfOperator(int firstIndex, int secondIndex) {
+        if(firstIndex != -1 || secondIndex != -1){
+
+            if(firstIndex == -1) return secondIndex;
+            if(secondIndex == -1) return firstIndex;
+
+            return firstIndex < secondIndex ? firstIndex : secondIndex;
+        }
+
+        return -1;
+    }
 
 }
--- src/test/java/EquationParserTest.java
+++ src/test/java/EquationParserTest.java
@@ -1,3 +1,4 @@
+import actions.base.BaseAction;
 import actions.base.TwoParamsAction;
 import actions.base.Value;
 import actions.basic.Div;
@@ -27,25 +28,48 @@ class EquationParserTest {
 
     @Test
     void sumTest(){
-        TwoParamsAction out = EquationParser.parse("5 + 12");
+        BaseAction out = EquationParser.parse("5 + 12");
         assertEquals(out, new Sum(new Value(5L), new Value(12L)));
     }
 
     @Test
     void divTest(){
-        TwoParamsAction out = EquationParser.parse("82  /7");
+        BaseAction out = EquationParser.parse("82  /7");
         assertEquals(out, new Div(new Value(82L), new Value(7L)));
     }
 
     @Test
     void mulTest(){
-        TwoParamsAction out = EquationParser.parse("3 * 97");
+        BaseAction out = EquationParser.parse("3 * 97");
         assertEquals(out, new Mul(new Value(3L), new Value(97L)));
     }
 
     @Test
     void subTest(){
-        TwoParamsAction out = EquationParser.parse("21- 32");
+        BaseAction out = EquationParser.parse("21- 32");
         assertEquals(out, new Sub(new Value(21L), new Value(32L)));
+    }
+
+    @Test
+    void complexEquationTest1(){
+        BaseAction out = EquationParser.parse("21*9 - 8 - 9/3 + 2");
+        assertEquals(out, new Sub(new Mul(new Value(21L),
+                                          new Value(9L)),
+                                  new Sub(new Value(8L),
+                                          new Sum(new Div(new Value(9L),
+                                                          new Value(3L)),
+                                                  new Value(2L)))));
+    }
+
+    @Test
+    void complexEquationTest2(){
+        BaseAction out = EquationParser.parse("13 *3- 2 - 6 / 2  +   1*  3");
+        assertEquals(out, new Sub(new Mul(new Value(13L),
+                                          new Value(3L)),
+                                  new Sub(new Value(2L),
+                                          new Sum(new Div(new Value(6L),
+                                                          new Value(2L)),
+                                                  new Mul(new Value(1L),
+                                                          new Value(3L))))));
     }
 }
